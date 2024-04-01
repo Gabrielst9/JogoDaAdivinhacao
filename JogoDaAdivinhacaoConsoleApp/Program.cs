1e5527af4f99448e76d2a9f417a7b2bf004ebfce
Gabriel Tavares <gabrielgstell@gmail.com>
2024-04-01 15:36:15 -0300

    Adicionar arquivos de projeto.

---
+++ JogoDaAdivinhacaoConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace JogoDaAdivinhacaoConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExibirTitulo();
+
+        }
+
+        #region Exibir titulo
+        private static void ExibirTitulo()
+        {
+            Console.WriteLine("------------------------");
+            Console.WriteLine("| Jogo da Adivinhação! |");
+            Console.WriteLine("------------------------\n");
+        }
+        #endregion
+    }
+}
